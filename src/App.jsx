ea0b8e15a4bbe2992d0e748598306945180c4cfe
adalsus <adalbertomonar@hotmail.com>
2023-04-04 09:58:55 -0500

    next commit : preparación preliminar como icono, logo, nombre para la app

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.jsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -21,5 +21,8 @@ function App() {
     </div>
   );
 }
+
+//Estudiante: Adalberto Monar devFT-M2
+
 
 export default App;
